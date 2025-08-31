243cb9cee253b66f7ec8bf1b7afa6d5f9f7c2912
Tuvudel <tuvalu144@gmail.com>
2025-09-01 01:14:55 +0300

    -first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/game.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'game.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 3000,
+    open: true
+  },
+  externals: {
+    phaser: 'Phaser'
+  }
+};
